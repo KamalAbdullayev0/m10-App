96fcca6bed491fe59931e4e0b5d04132a9fe99ec
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2025-02-02 20:56:15 +0400

    Initial Commit

---
+++ M10-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M10-App
+//
+//  Created by Kamal Abdullayev on 02.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
